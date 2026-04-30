3699ad8553df29c4a64412fe2c87a0aa31a6926d
manueldeveloper99 <manueldeveloper99cs04@gmail.com>
2026-04-29 19:09:00 -0600

    agregando

--- student-api/src/main/java/com/example/student_api/StudentApiApplication.java
+++ student-api/src/main/java/com/example/student_api/StudentApiApplication.java
@@ -10,5 +10,5 @@ public class StudentApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApiApplication.class, args);
 	}
-	// changes
+	// changes 2 change
 }
